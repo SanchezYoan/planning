862f8a20d9276d985f4ed40f69d88ab045dfd9b4
sanchez yoan <yoan30470@hotmail.fr>
2021-05-06 10:17:05 +0200

    second

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from datetime import datetime
 
 
-def read_plan():
+def read_plan(file):
 
-    with open("planning_log.py", r, "utf_8") as p:
-        planning = p.readlines()
-        ord(planning)
+    with open(file, "r") as f:
+        planning = f.readline()
+        while planning:
+            planning = f.readline()
+            newline = planning.strip().split()
+            if planning != []:
+                print(newline)
+
+# read_plan("planning.log")
 
 
-def pourcent():
-
-    with open("expected_output.txt", w, "utf_8") as f:
-
-
-read_plan()
+def pourcent(start, end):
+    read_plan("planning.log")
+    with open("expected_output.txt", "r") as f:
+        # s1 = newline[0]
+        # s2 = newline[1]
+        FM = '%H:%M'
+        tdelta = datetime.strptime(end, FM) - datetime.strptime(start, FM)
+pourcent('11:30', '12:30')
